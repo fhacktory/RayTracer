a0781ef62ae7b2401d638e21488427d88e656caf
Tran_a <a.tran@student.isartdigital.com>
2014-10-19 01:05:30 +0200

    Objects Files created

---
+++ RayTracerSolution/RayTracer/Headers/Plane.h
@@ -0,0 +1,23 @@
+#pragma once
+
+class Object
+{
+	/*	VARIABLES	*/
+
+private:
+protected:
+public:
+
+	/*	FUNCTIONS	*/
+
+private:
+	void	destroy();
+protected:
+public:
+	Object();
+	~Object();
+	Object(const Object& right);
+	Object	operator=(const Object& right);
+
+	bool	initialize();
+};

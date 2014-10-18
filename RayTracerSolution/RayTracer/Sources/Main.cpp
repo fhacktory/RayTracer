159bd744c1d07c145a8c95e7459897f6503bbae4
Paul-Emile Le Reun <paulemile.lereun@gmail.com>
2014-10-18 18:40:59 +0200

    Playing with SFML now

--- RayTracerSolution/RayTracer/Sources/Main.cpp
+++ RayTracerSolution/RayTracer/Sources/Main.cpp
@@ -1,6 +1,25 @@
 #include <Windows.h>
+#include <SFML/Graphics.hpp>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pSCmdLine, int iCmdShow)
+int CALLBACK WinMain(_In_  HINSTANCE hInstance, _In_  HINSTANCE hPrevInstance, _In_  LPSTR lpCmdLine, _In_  int nCmdShow)
 {
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
 	return 0;
 }
